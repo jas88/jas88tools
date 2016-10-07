2fba62bdefaeb2dd0a014531a4650b29f0f17fdd
James A Sutherland <j@sutherland.pw>
2016-10-07 11:06:12 +0100

    Auto-updater script

---
+++ uadtools/update.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Auto-update script - to be run from Cron on hacklab1/Hannah
+# Pulls latest boot image and management scripts from JS
+
+# Dependencies:
+# HTTP daemon on :80 serving /lab/boot as :80/boot, including PHP
+# tftp sha256sum curl
+
+set -e
+mkdir /tmp/$$
+cd /lab/boot
+curl -s https://us.deadnode.org/tc.xz.sha256 > /tmp/$$/sums
+if ! sha256sum --status -c /tmp/$$/sums
+then
+	cd /tmp/$$
+	curl -s https://us.deadnode.org/tc.xz > tc.xz
+	curl -s https://us.deadnode.org/boot.txt > boot.php
+	sha256sum --status -c sums
+	xzcat tc.xz > /dev/null
+	php -l boot.php > /dev/null
+	mv tc.xz boot.php /lab/boot/
+fi
+rm /tmp/$$/sums
+rmdir /tmp/$$
