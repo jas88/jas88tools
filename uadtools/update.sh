a0dcdfd2d044346f6d052454f0752f075c765291
James A Sutherland <j@sutherland.pw>
2016-10-07 11:24:54 +0100

    Use .txt extension on PHP as easy download route

--- uadtools/update.sh
+++ uadtools/update.sh
@@ -15,11 +15,12 @@ if ! sha256sum --status -c /tmp/$$/sums
 then
 	cd /tmp/$$
 	curl -s https://us.deadnode.org/tc.xz > tc.xz
-	curl -s https://us.deadnode.org/boot.txt > boot.php
+	curl -s https://us.deadnode.org/boot.txt > boot.txt
 	sha256sum --status -c sums
 	xzcat tc.xz > /dev/null
-	php -l boot.php > /dev/null
-	mv tc.xz boot.php /lab/boot/
+	php -l boot.txt > /dev/null
+	mv tc.xz /lab/boot/
+	mv boot.txt /lab/boot/boot.php
 fi
 rm /tmp/$$/sums
 rmdir /tmp/$$
